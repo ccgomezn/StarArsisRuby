dd2e2dc7cf4bc4a3645536245fd1d598793808ac
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-09-23 01:48:38 -0500

    assets

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module AppStar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.serve_static_assets = true
     config.assets.enabled = true
 	Rails.application.config.assets.paths << "#{Rails.root}/app/assets/javascripts/lib/vector-map/"
 	Rails.application.config.assets.paths << "#{Rails.root}/app/assets/javascripts/lib/vector-map/country/"
